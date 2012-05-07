b549d985dedc9191b734072fbfac362a565a4db6
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2012-05-07 23:31:34 +0200

    can now toggle betweens screens with alt+w

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,6 +6,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Actions.FindEmptyWorkspace
 import XMonad.Config.Gnome
+import XMonad.Actions.CycleWS
 
 myManageHook = composeAll[
   className =? "Emesene"      --> doShift "2:com",
@@ -23,6 +24,7 @@ main = do
   xmonad $ defaultConfig{
     workspaces = ["1:dev", "2:com", "3:web", "4:file",
     "5:mail", "6", "7", "8", "9:mail", "0", "-", "="],
+
     manageHook = myManageHook <+> manageHook defaultConfig,
     layoutHook = avoidStruts $ layoutHook defaultConfig,
     borderWidth         = 1,
@@ -30,9 +32,10 @@ main = do
     normalBorderColor   = "#000000",
     focusedBorderColor  = "#9d9d9d",
     focusFollowsMouse   = False
-  }`additionalKeys`
-    [ ((mod4Mask,               xK_f),      spawn "thunar")
-    , ((0,                      xK_Print),  spawn "scrot")
-    , ((mod4Mask,               xK_n),      viewEmptyWorkspace)
-    , ((mod4Mask .|. shiftMask, xK_n),      tagToEmptyWorkspace)
+  }`additionalKeys`[
+      ((mod1Mask,               xK_f),      spawn "thunar"),
+      ((0,                      xK_Print),  spawn "scrot"),
+      ((mod1Mask,               xK_n),      viewEmptyWorkspace),
+      ((mod1Mask .|. shiftMask, xK_n),      tagToEmptyWorkspace),
+      ((mod1Mask,               xK_w),      nextScreen)
     ]
