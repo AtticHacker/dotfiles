80e72b3cbd8edcf3214aae3873f58d3911750690
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2013-07-03 15:36:16 +0200

    cleaned up xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,41 +1,41 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 import XMonad.Actions.FindEmptyWorkspace
-import XMonad.Config.Gnome
 import XMonad.Actions.CycleWS
 import Graphics.X11.ExtraTypes.XF86
 import qualified XMonad.StackSet as W
 import XMonad.Layout.ToggleLayouts
-import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
 import XMonad.Actions.NoBorders
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Config.Xfce
+import Data.Monoid
+import XMonad.Layout.LayoutModifier
 
-xF86XK_TouchpadToggle :: KeySym
-xF86XK_TouchpadToggle = 0x1008ffa9
+myManageHook :: Query (Endo WindowSet)
+myManageHook = composeAll
+    [ className =? "Skype"       --> doShift "2"
+    , className =? "Pidgin"      --> doShift "2"
+    , className =? "Firefox"     --> doShift "3"
+    , className =? "Chromium"    --> doShift "3"
+    , className =? "Thunar"      --> doShift "4"
+    , className =? "Thunderbird" --> doShift "5"
+    , className =? "Turpial"     --> doShift "5"
+    , className =? "Screenkey"   --> doIgnore
+    ]
 
-
-myManageHook = composeAll [ className =? "Skype"        --> doShift "2"
-                          , className =? "Pidgin"       --> doShift "2"
-                          , className =? "Firefox"	--> doShift "3"
-                          , className =? "Chromium"	--> doShift "3"
-                          , className =? "Thunar"       --> doShift "4"
-                          , className =? "Thunderbird"	--> doShift "5"
-                          , className =? "Turpial"	--> doShift "5"
-                          , className =? "Screenkey"    --> doIgnore
-                          ]
 main :: IO ()
 main = xmonad $ xfceLayout
 
+xfceLayout :: XConfig (ToggleLayouts (ModifiedLayout WithBorder Full)
+                      (ModifiedLayout AvoidStruts
+                      (ModifiedLayout AvoidStruts
+                      (Choose Tall (Choose (Mirror Tall) Full)))))
+
 xfceLayout = xfceConfig
              { modMask = mod4Mask
-             , workspaces = [ "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0" , "-" , "=" ]
-             , manageHook = myManageHook <+> manageHook xfceConfig --myManageDocks <+> manageHook defaultConfig
+             , workspaces = map show ([1..9] :: [Int])
+             , manageHook = myManageHook <+> manageHook xfceConfig
              , borderWidth        = 1
              , terminal           = "urxvt"
              , normalBorderColor  = "#000000"
@@ -44,231 +44,89 @@ xfceLayout = xfceConfig
                             avoidStruts $ layoutHook xfceConfig
              } `additionalKeys` addKeys
 
-
 addKeys :: [((KeyMask, KeySym), X ())]
-addKeys = [ ((0, xF86XK_AudioRaiseVolume     ), spawn "amixer --quiet set Master 1+")
-          , ((0, xF86XK_AudioLowerVolume     ), spawn "amixer --quiet set Master 1-")
-          , ((0, xF86XK_AudioMute            ), spawn "amixer --quiet set Master toggle")
-          , ((0, xF86XK_KbdBrightnessUp      ), spawn "asus-kbd-backlight up")
-          , ((0, xF86XK_KbdBrightnessDown    ), spawn "asus-kbd-backlight down")
---          , ((0, xF86XK_TouchpadToggle       ), spawn "synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')")
-          , ((mod4, xK_F9                    ), spawn "synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')")
-          , ((mod4, xK_F5                    ), spawn "asus-screen-brightness down")
-          , ((mod4, xK_F6                    ), spawn "asus-screen-brightness up")
-          , ((mod4, xK_f                     ), spawn "thunar")
-          , ((mod4 .|. shiftMask, xK_m       ), spawn "terminal")
-          , ((0, xK_Print                    ), spawn "scrot")
-          , ((mod4, xK_Print                 ), spawn "xfce4-screenshooter -r")
-          , ((mod4, xK_n                     ), viewEmptyWorkspace)
-          , ((mod4 .|. shiftMask, xK_n       ), tagToEmptyWorkspace)
-          -- , ((modMask .|. shiftMask, xK_3 ), spawn "scrot")
-          -- , ((modMask .|. shiftMask, xK_4 ), spawn "shutter -s")
-          , ((mod4, xK_b                     ), spawn "toggleXmobar")
-          , ((mod4, xK_w                     ), nextScreen)
-          , ((mod1Mask, xK_Tab               ), windows W.focusDown)
-          , ((mod4 .|. shiftMask, xK_x       ), sendMessage ToggleLayout)
-          , ((mod4, xK_x                     ), withFocused toggleBorder)
-          , ((mod4, xK_q                     ), undefined)
-          , ((mod4, xK_p                     ), spawn "dmenu_run")
-          , ((mod4, xK_d                     ), spawn "xfce4-dict")
+addKeys =
+    [ ((0, xF86XK_AudioRaiseVolume ), spawn "amixer --quiet set Master 1+")
+    , ((0, xF86XK_AudioLowerVolume ), spawn "amixer --quiet set Master 1-")
+    , ((0, xF86XK_AudioMute        ), spawn "amixer --quiet set Master toggle")
+    , ((0, xF86XK_KbdBrightnessUp  ), spawn "asus-kbd-backlight up")
+    , ((0, xF86XK_KbdBrightnessDown), spawn "asus-kbd-backlight down")
+    , ((mod4, xK_F9                ), spawn disableTouch)
+    , ((mod4, xK_F5                ), spawn "asus-screen-brightness down")
+    , ((mod4, xK_F6                ), spawn "asus-screen-brightness up")
+    , ((mod4, xK_f                 ), spawn "thunar")
+    , ((mod4 .|. shiftMask, xK_m   ), spawn "terminal")
+    , ((0, xK_Print                ), spawn "scrot")
+    , ((mod4, xK_Print             ), spawn "xfce4-screenshooter -r")
+    , ((mod4, xK_n                 ), viewEmptyWorkspace)
+    , ((mod4 .|. shiftMask, xK_n   ), tagToEmptyWorkspace)
+    , ((mod4, xK_b                 ), spawn "toggleXmobar")
+    , ((mod4, xK_w                 ), nextScreen)
+    , ((mod1Mask, xK_Tab           ), windows W.focusDown)
+    , ((mod4 .|. shiftMask, xK_x   ), sendMessage ToggleLayout)
+    , ((mod4, xK_x                 ), withFocused toggleBorder)
+    , ((mod4, xK_q                 ), undefined)
+    , ((mod4, xK_p                 ), spawn "dmenu_run")
+    , ((mod4, xK_d                 ), spawn "xfce4-dict")
+    ] where mod4         = mod4Mask
+            disableTouch = "synclient TouchpadOff=$(synclient -l" ++
+                           "| grep -c 'TouchpadOff.*=.*0')"
 
-          ] where mod4 = mod4Mask
+{- Available keys
+| <Backspace>    | <Tab>         | <Return>      | <Pause>         |
+| <Scroll_lock>  | <Sys_Req>     | <Print>       | <Escape>, <Esc> |
+| <Delete>       | <Home>        | <Left>, <L>   | <Up>, <U>       |
+| <Right>, <R>   | <Down>, <D>   | <Page_Up>     | <Page_Down>     |
+| <End>          | <Insert>      | <Break>       | <Space>         |
+| <F1>-<F24>     | <KP_Space>    | <KP_Tab>      | <KP_Enter>      |
+| <KP_F1>        | <KP_F2>       | <KP_F3>       | <KP_F4>         |
+| <KP_Home>      | <KP_Left>     | <KP_Up>       | <KP_Right>      |
+| <KP_Down>      | <KP_Prior>    | <KP_Page_Up>  | <KP_Next>       |
+| <KP_Page_Down> | <KP_End>      | <KP_Begin>    | <KP_Insert>     |
+| <KP_Delete>    | <KP_Equal>    | <KP_Multiply> | <KP_Add>        |
+| <KP_Separator> | <KP_Subtract> | <KP_Decimal>  | <KP_Divide>     |
+| <KP_0>-<KP_9>  |               |               |                 |
 
-
-
-
-    -- , layoutHook         = toggleLayouts (noBorders Full) $
-    --                        avoidStruts $ layoutHook defaultConfig
-    -- }
---myKeys conf@(XConfig { XMonad.modMask = modMask } ) = M.fromList $
-
--- > <Backspace>
--- > <Tab>
--- > <Return>
--- > <Pause>
--- > <Scroll_lock>
--- > <Sys_Req>
--- > <Print>
--- > <Escape>, <Esc>
--- > <Delete>
--- > <Home>
--- > <Left>, <L>
--- > <Up>, <U>
--- > <Right>, <R>
--- > <Down>, <D>
--- > <Page_Up>
--- > <Page_Down>
--- > <End>
--- > <Insert>
--- > <Break>
--- > <Space>
--- > <F1>-<F24>
--- > <KP_Space>
--- > <KP_Tab>
--- > <KP_Enter>
--- > <KP_F1>
--- > <KP_F2>
--- > <KP_F3>
-
-
-
-
-
-
-
-
-
-
-
-
-
--- > <KP_F4>
--- > <KP_Home>
--- > <KP_Left>
--- > <KP_Up>
--- > <KP_Right>
--- > <KP_Down>
--- > <KP_Prior>
--- > <KP_Page_Up>
--- > <KP_Next>
--- > <KP_Page_Down>
--- > <KP_End>
--- > <KP_Begin>
--- > <KP_Insert>
--- > <KP_Delete>
--- > <KP_Equal>
--- > <KP_Multiply>
--- > <KP_Add>
--- > <KP_Separator>
--- > <KP_Subtract>
--- > <KP_Decimal>
--- > <KP_Divide>
--- > <KP_0>-<KP_9>
---
--- Long list of multimedia keys. Please note that not all keys may be
--- present in your particular setup although most likely they will do.
---
--- > <XF86ModeLock>
--- > <XF86MonBrightnessUp>
--- > <XF86MonBrightnessDown>
--- > <XF86KbdLightOnOff>
--- > <XF86KbdBrightnessUp>
--- > <XF86KbdBrightnessDown>
--- > <XF86Standby>
--- > <XF86AudioLowerVolume>
--- > <XF86AudioMute>
--- > <XF86AudioRaiseVolume>
--- > <XF86AudioPlay>
--- > <XF86AudioStop>
--- > <XF86AudioPrev>
--- > <XF86AudioNext>
--- > <XF86HomePage>
--- > <XF86Mail>
--- > <XF86Start>
--- > <XF86Search>
--- > <XF86AudioRecord>
--- > <XF86Calculator>
--- > <XF86Memo>
--- > <XF86ToDoList>
--- > <XF86Calendar>
--- > <XF86PowerDown>
--- > <XF86ContrastAdjust>
--- > <XF86RockerUp>
--- > <XF86RockerDown>
--- > <XF86RockerEnter>
--- > <XF86Back>
--- > <XF86Forward>
--- > <XF86Stop>
--- > <XF86Refresh>
--- > <XF86PowerOff>
--- > <XF86WakeUp>
--- > <XF86Eject>
--- > <XF86ScreenSaver>
--- > <XF86WWW>
--- > <XF86Sleep>
--- > <XF86Favorites>
--- > <XF86AudioPause>
--- > <XF86AudioMedia>
--- > <XF86MyComputer>
--- > <XF86VendorHome>
--- > <XF86LightBulb>
--- > <XF86Shop>
--- > <XF86History>
--- > <XF86OpenURL>
--- > <XF86AddFavorite>
--- > <XF86HotLinks>
--- > <XF86BrightnessAdjust>
--- > <XF86Finance>
--- > <XF86Community>
--- > <XF86AudioRewind>
--- > <XF86XF86BackForward>
--- > <XF86Launch0>-<XF86Launch9>, <XF86LaunchA>-<XF86LaunchF>
--- > <XF86ApplicationLeft>
--- > <XF86ApplicationRight>
--- > <XF86Book>
--- > <XF86CD>
--- > <XF86Calculater>
--- > <XF86Clear>
--- > <XF86Close>
--- > <XF86Copy>
--- > <XF86Cut>
--- > <XF86Display>
--- > <XF86DOS>
--- > <XF86Documents>
--- > <XF86Excel>
--- > <XF86Explorer>
--- > <XF86Game>
--- > <XF86Go>
--- > <XF86iTouch>
--- > <XF86LogOff>
--- > <XF86Market>
--- > <XF86Meeting>
--- > <XF86MenuKB>
--- > <XF86MenuPB>
--- > <XF86MySites>
--- > <XF86New>
--- > <XF86News>
--- > <XF86OfficeHome>
--- > <XF86Open>
--- > <XF86Option>
--- > <XF86Paste>
--- > <XF86Phone>
--- > <XF86Q>
--- > <XF86Reply>
--- > <XF86Reload>
--- > <XF86RotateWindows>
--- > <XF86RotationPB>
--- > <XF86RotationKB>
--- > <XF86Save>
--- > <XF86ScrollUp>
--- > <XF86ScrollDown>
--- > <XF86ScrollClick>
--- > <XF86Send>
--- > <XF86Spell>
--- > <XF86SplitScreen>
--- > <XF86Support>
--- > <XF86TaskPane>
--- > <XF86Terminal>
--- > <XF86Tools>
--- > <XF86Travel>
--- > <XF86UserPB>
--- > <XF86User1KB>
--- > <XF86User2KB>
--- > <XF86Video>
--- > <XF86WheelButton>
--- > <XF86Word>
--- > <XF86Xfer>
--- > <XF86ZoomIn>
--- > <XF86ZoomOut>
--- > <XF86Away>
--- > <XF86Messenger>
--- > <XF86WebCam>
--- > <XF86MailForward>
--- > <XF86Pictures>
--- > <XF86Music>
--- > <XF86TouchpadToggle>
--- > <XF86_Switch_VT_1>-<XF86_Switch_VT_12>
--- > <XF86_Ungrab>
--- > <XF86_ClearGrab>
--- > <XF86_Next_VMode>
--- > <XF86_Prev_VMode>
+| <XF86ModeLock>         | <XF86MonBrightnessUp>  | <XF86MonBrightnessDown> |
+| <XF86KbdLightOnOff>    | <XF86KbdBrightnessUp>  | <XF86KbdBrightnessDown> |
+| <XF86Standby>          | <XF86AudioLowerVolume> | <XF86AudioMute>         |
+| <XF86AudioRaiseVolume> | <XF86AudioPlay>        | <XF86AudioStop>         |
+| <XF86AudioPrev>        | <XF86AudioNext>        | <XF86HomePage>          |
+| <XF86Mail>             | <XF86Start>            | <XF86Search>            |
+| <XF86AudioRecord>      | <XF86Calculator>       | <XF86Memo>              |
+| <XF86ToDoList>         | <XF86Calendar>         | <XF86PowerDown>         |
+| <XF86ContrastAdjust>   | <XF86RockerUp>         | <XF86RockerDown>        |
+| <XF86RockerEnter>      | <XF86Back>             | <XF86Forward>           |
+| <XF86Stop>             | <XF86Refresh>          | <XF86PowerOff>          |
+| <XF86WakeUp>           | <XF86Eject>            | <XF86ScreenSaver>       |
+| <XF86WWW>              | <XF86Sleep>            | <XF86Favorites>         |
+| <XF86AudioPause>       | <XF86AudioMedia>       | <XF86MyComputer>        |
+| <XF86VendorHome>       | <XF86LightBulb>        | <XF86Shop>              |
+| <XF86History>          | <XF86OpenURL>          | <XF86AddFavorite>       |
+| <XF86HotLinks>         | <XF86BrightnessAdjust> | <XF86Finance>           |
+| <XF86Community>        | <XF86AudioRewind>      | <XF86XF86BackForward>   |
+| <XF86Launch0-9>        | <XF86LaunchA-F>        | <XF86ApplicationLeft>   |
+| <XF86ApplicationRight> | <XF86Book>             | <XF86CD>                |
+| <XF86Calculater>       | <XF86Clear>            | <XF86Close>             |
+| <XF86Copy>             | <XF86Cut>              | <XF86Display>           |
+| <XF86DOS>              | <XF86Documents>        | <XF86Excel>             |
+| <XF86Explorer>         | <XF86Game>             | <XF86Go>                |
+| <XF86iTouch>           | <XF86LogOff>           | <XF86Market>            |
+| <XF86Meeting>          | <XF86MenuKB>           | <XF86MenuPB>            |
+| <XF86MySites>          | <XF86New>              | <XF86News>              |
+| <XF86OfficeHome>       | <XF86Open>             | <XF86Option>            |
+| <XF86Paste>            | <XF86Phone>            | <XF86Q>                 |
+| <XF86Reply>            | <XF86Reload>           | <XF86RotateWindows>     |
+| <XF86RotationPB>       | <XF86RotationKB>       | <XF86Save>              |
+| <XF86ScrollUp>         | <XF86ScrollDown>       | <XF86ScrollClick>       |
+| <XF86Send>             | <XF86Spell>            | <XF86SplitScreen>       |
+| <XF86Support>          | <XF86TaskPane>         | <XF86Terminal>          |
+| <XF86Tools>            | <XF86Travel>           | <XF86UserPB>            |
+| <XF86User1KB>          | <XF86User2KB>          | <XF86Video>             |
+| <XF86WheelButton>      | <XF86Word>             | <XF86Xfer>              |
+| <XF86ZoomIn>           | <XF86ZoomOut>          | <XF86Away>              |
+| <XF86Messenger>        | <XF86WebCam>           | <XF86MailForward>       |
+| <XF86Pictures>         | <XF86Music>            | <XF86TouchpadToggle>    |
+| <XF86_Switch_VT_1-12>  | <XF86_Ungrab>          | <XF86_ClearGrab>        |
+| <XF86_Next_VMode>      | <XF86_Prev_VMode>      |                         |
+-}
